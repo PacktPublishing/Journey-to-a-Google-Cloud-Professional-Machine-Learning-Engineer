fd85e24ca2f0baecdf15c455bb1fb687eca15b84
lsong66888 <62619422+lsong66888@users.noreply.github.com>
2022-08-12 08:20:54 -0500

    Create eggcount.py

---
+++ chapter2/eggcount.py
@@ -0,0 +1,5 @@
+for i in range (1, 2001, 2): 
+
+ if ( ((i % 3) == 0) and ((i % 4) == 1) and ((i % 5) == 4) and ((i % 6) == 3) and ((i % 7) == 0) and ((i % 8) == 1) and((i % 9) == 0) ): 
+
+    print("Total number of eggs in the bucket can be %d" %i)
