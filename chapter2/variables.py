0c1e89c35da63afe79a3e95e0e0b02ad1e18f6cf
lsong66888 <62619422+lsong66888@users.noreply.github.com>
2022-08-12 08:26:14 -0500

    Create variables.py

---
+++ chapter2/variables.py
@@ -0,0 +1,18 @@
+x = int(2.8)
+print(x) 
+
+z = str(3.0)
+z = float(z)
+z = int(z/2)
+z = z * 4.5 
+print(z)  
+
+z = z ** x 
+print (z) 
+
+y = float("3")
+a = (x==y) 
+print (a) 
+
+b = ((z==z) and (a != x)) 
+print (b) 
