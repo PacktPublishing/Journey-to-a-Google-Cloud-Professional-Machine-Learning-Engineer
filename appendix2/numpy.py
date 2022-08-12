c5a5a85d426f2ef5a119f33de20b3a8494152a28
lsong66888 <62619422+lsong66888@users.noreply.github.com>
2022-08-12 15:36:21 -0500

    Create numpy.py

---
+++ appendix2/numpy.py
@@ -0,0 +1,69 @@
+import numpy as np 
+my_list = [1,2,3] 
+my_list 
+
+my_list = [1,2,3] 
+arr = np.array(my_list) 
+arr 
+
+my_mat =[[10,20,30],[40,50,60],[70,80,90]] 
+np.array(my_mat) 
+
+np.arange(0,10) 
+
+np.arange(0,11,2) 
+
+np.zeros(5) 
+
+np.zeros((4,4)) 
+
+np.ones((3,4)) 
+
+np.linspace(0,5,10) 
+
+np.linspace(0,5,25) 
+np.eye(5) 
+
+np.random.rand(4,5) 
+
+np.random.randn(4) 
+
+np.random.randint(1,100) 
+
+np.random.randint(1,100,10) 
+
+array = np.arange(25) 
+array.reshape(5,5) 
+
+array = np.random.randint(0,50,10) 
+array 
+array.max() 
+array.min() 
+
+array = np.random.randint(0,50,10) 
+array 
+array[3] 
+array[0:4] 
+
+array = np.random.randint(0,50,100) 
+array 
+
+array[0:3] = 100 
+array 
+
+array = np.random.randint(0,50,25).reshape(5,5) 
+array 
+array[0:3] 
+
+array = np.random.randint(0,50,25).reshape(5,5) 
+array 
+array > 25 
+
+array = np.arange(10) 
+array 
+
+array + array 
+
+array * 2 
+
+np.sin(array) 
